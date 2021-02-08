e2aa88b046e7a3670ec10d6d21ee02471b0c65e7
Colin-Kohli <75268139+Colin-Kohli@users.noreply.github.com>
2021-02-08 16:57:48 -0500

    First draft of main function complete
    Added ability to collect and parse feed, returning URLs which have not been updated in x amount of days.

--- rss_inactivity.py
+++ rss_inactivity.py
@@ -1,12 +1,9 @@
+from re import split
 import unittest
 import pip
-
-
-def import_or_install_packages():
-    try:
-        __import__('feedparser')
-    except ImportError:
-        pip.main(['install', 'feedparser'])
+import feedparser
+import datetime
+import logging
 
 
 def get_rss_inactivity(rss_dict, minimum_days_inactive):
@@ -27,16 +24,54 @@ def get_rss_inactivity(rss_dict, minimum_days_inactive):
     # Assert all function arguments to be passed in as expected
     sanity_check(rss_dict, minimum_days_inactive)
 
-    # Import packages needed for this program, and install them if they are not present
-    import_or_install_packages()
-
     # A blank dictionary which will eventually hold company names paired with URLs which have not been updated in 'minimium_days_inactive' days
     inactive_companies = {}
 
+    # Will will capture the date just once and pass it to other functions rather than calling it every time we need to do a date calculation
+    today = datetime.datetime.today()
+
     for company in rss_dict:
         feed_urls = rss_dict[company]
+        for url in feed_urls:
+            feedFound = False
+            try:
+                feed = feedparser.parse(url)
+                feedFound = True
+            except:
+                logging.warn(
+                    "Exception while parsing feed. If the root certificates of your default python installation have not been updated, please install new certificates")
+
+            # Dont love the structure here, will try to find a way to reorder this exception block
+
+            if(feedFound):
+                days_inactive = days_since_last_update(feed, today)
+                if days_inactive >= minimum_days_inactive:
+                    if company in inactive_companies:
+                        inactive_companies[company].append(url)
+                    else:
+                        inactive_companies[company] = [url]
 
     return inactive_companies
+
+
+def days_since_last_update(feed, today):
+
+    # NEED TO CATCH IF DATE ITEM DOESNT EXIST LIKE WITH THAT ONE PODCAST
+
+    # dates have a strange format, with items seperated by spaces. split the items by spaces, then build a datetime object so we can do date arithmetic
+    split_date = feed["feed"]["published"].split(" ")
+    day = split_date[1]
+    if len(day) == 1:
+        day = "0" + day  # the number of digits on the day of the month is irregular, so add a leading zero if just one digit
+    month = split_date[2]
+    year = split_date[3]
+    hour_minute_second = split_date[4]
+
+    last_post_date = datetime.datetime.strptime(
+        day + month + year + hour_minute_second, "%d%b%Y%H:%M:%S")  # build datetime object
+
+    # return day difference between most recent date and today
+    return (today - last_post_date).days
 
 
 def sanity_check(rss_dict, minimum_days_inactive):
@@ -63,4 +98,5 @@ def sanity_check(rss_dict, minimum_days_inactive):
     assert minimum_days_inactive >= 0, "Minimum days inactaive must be a non-negative integer"
 
 
-get_rss_inactivity({'a': ['1']}, 1)
+print(get_rss_inactivity(
+    {'Unraveled': ['https://rss.acast.com/unraveled']}, 10))

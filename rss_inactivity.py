0cf5ef92be9183015136eeacd99d1afe93d91410
Colin-Kohli <75268139+Colin-Kohli@users.noreply.github.com>
2021-02-08 15:01:18 -0500

    split asserts into sanity check function for readability
    Asserts are now their own seperate function. Unit tests are not passing assertRaised on AssertionExceptions, but asserts are catching bad input so far.

--- rss_inactivity.py
+++ rss_inactivity.py
@@ -1,3 +1,14 @@
+import unittest
+import pip
+
+
+def import_or_install_packages():
+    try:
+        __import__('feedparser')
+    except ImportError:
+        pip.main(['install', 'feedparser'])
+
+
 def get_rss_inactivity(rss_dict, minimum_days_inactive):
     '''
      Identifies companies which have had no rss activity for a given number of days. 
@@ -7,12 +18,49 @@ def get_rss_inactivity(rss_dict, minimum_days_inactive):
     The return dictionary is keyed by companies whose feeds have lapsed for the given amount of days, and valued by the respective URLs of those feeds.
 
     Parameters:
-    rss_dict (dict of str: str): A dictionary keyed from the string of a company's name and valued to some amount of RSS feed URL strings.
+    rss_dict (dict of str: list[str]): A dictionary keyed from the string of a company's name and valued to a list of some amount of RSS feed URL strings.
 
     miniumum_days_inactive (int): The number of days a feed must be inactive for to be returned by the function. 
 
     '''
 
+    # Assert all function arguments to be passed in as expected
+    sanity_check(rss_dict, minimum_days_inactive)
+
+    # Import packages needed for this program, and install them if they are not present
+    import_or_install_packages()
+
+    # A blank dictionary which will eventually hold company names paired with URLs which have not been updated in 'minimium_days_inactive' days
     inactive_companies = {}
 
+    for company in rss_dict:
+        feed_urls = rss_dict[company]
+
     return inactive_companies
+
+
+def sanity_check(rss_dict, minimum_days_inactive):
+    assert isinstance(
+        rss_dict, dict), "Argument of wrong type! Type 'dictionary' required as first argument for fuction get_rss_inactivity"
+
+    assert rss_dict != {}, "First argument 'rss_dict' should not be empty"
+
+    assert isinstance(
+        rss_dict, dict), "Argument of wrong type! Type 'dictionary' required as first argument for fuction get_rss_inactivity"
+
+    assert isinstance(rss_dict[list(rss_dict.keys())[
+        0]], list), "Argument of wrong type! Type 'List' required as value argument for dictionary 'rss_dict'"
+
+    assert rss_dict[list(rss_dict.keys())[0]] != [
+    ], "Value list in 'rss_dict' should not be empty"
+
+    assert isinstance(rss_dict[list(rss_dict.keys())[
+        0]][0], str), "Argument of wrong type! Type 'String' required as item in value list for dictionary 'rss_dict'"
+
+    assert isinstance(minimum_days_inactive,
+                      int), "Argument of wrong type! Type 'int' required as second argument for fuction get_rss_inactivity"
+
+    assert minimum_days_inactive >= 0, "Minimum days inactaive must be a non-negative integer"
+
+
+get_rss_inactivity({'a': ['1']}, 1)
--- test_rss_inactivity.py
+++ test_rss_inactivity.py
@@ -1,2 +1,21 @@
 import unittest
-import rss_inactivity
+import rss_inactivity as rss
+
+
+class TestRSS(unittest.TestCase):
+
+    def test_ReturnDictIsBlank_False(self):
+        self.assertNotEqual(rss.get_rss_inactivity({'a': ['1']}, 1), {})
+
+    # def test_BadInput_AssertionErrorThrown(self):
+    #     self.assertRaises(
+    #         AssertionError, rss.get_rss_inactivity({'a': ["6"]}, 1))
+    #     self.assertRaises(AssertionError, rss.get_rss_inactivity({[]: 5}, 1))
+
+
+    # TODO
+    # Test if urls and companies are paired correctly
+    # Test multiple feeds per company, some lapsed some not
+    # assert url regex
+if __name__ == '__main__':
+    unittest.main()

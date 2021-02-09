702c81af552cf6dc0078ed7384ab8259d0234148
Colin-Kohli <75268139+Colin-Kohli@users.noreply.github.com>
2021-02-08 19:53:34 -0500

    Changed date handling, added more unit tests and exception handling
    Date is now a default argument in order to regularize testing. Added handling of irregular XML formats.
    Added and cleaned unit tests.

--- rss_inactivity.py
+++ rss_inactivity.py
@@ -6,29 +6,27 @@ import datetime
 import logging
 
 
-def get_rss_inactivity(rss_dict, minimum_days_inactive):
+def get_rss_inactivity(rss_dict, minimum_days_inactive, today=datetime.datetime.today()):
     '''
-     Identifies companies which have had no rss activity for a given number of days. 
+     Identifies companies which have had no rss activity for a given number of days.
 
-    This function takes a dictionary from company names to company RSS feed, and selects the companies which have not added to their feed in some number of days. 
-    As the return type is unspecified by the buisness requirments, the function will return a dictionary. 
+    This function takes a dictionary from company names to company RSS feed, and selects the companies which have not added to their feed in some number of days.
+    As the return type is unspecified by the buisness requirments, the function will return a dictionary.
     The return dictionary is keyed by companies whose feeds have lapsed for the given amount of days, and valued by the respective URLs of those feeds.
 
     Parameters:
     rss_dict (dict of str: list[str]): A dictionary keyed from the string of a company's name and valued to a list of some amount of RSS feed URL strings.
 
-    miniumum_days_inactive (int): The number of days a feed must be inactive for to be returned by the function. 
+    miniumum_days_inactive (int): The number of days a feed must be inactive for to be returned by the function.
 
+    today (datetime): A datetime object representing the current time for the caller. Should generally be left as default, and is used mostly for testing consistency across days. 
+        Current datetime is declared ahead of time to avoid multiple calls every time we need to do a date calculation.
     '''
-
     # Assert all function arguments to be passed in as expected
     sanity_check(rss_dict, minimum_days_inactive)
 
     # A blank dictionary which will eventually hold company names paired with URLs which have not been updated in 'minimium_days_inactive' days
     inactive_companies = {}
-
-    # Will will capture the date just once and pass it to other functions rather than calling it every time we need to do a date calculation
-    today = datetime.datetime.today()
 
     for company in rss_dict:
         feed_urls = rss_dict[company]
@@ -36,37 +34,48 @@ def get_rss_inactivity(rss_dict, minimum_days_inactive):
             feedFound = False
             try:
                 feed = feedparser.parse(url)
-                feedFound = True
             except:
                 logging.warn(
                     "Exception while parsing feed. If the root certificates of your default python installation have not been updated, please install new certificates")
+                continue
 
-            # Dont love the structure here, will try to find a way to reorder this exception block
+            days_inactive = days_since_last_update(feed, today)
+            if days_inactive == KeyError:  # Catch URL formats without a date item
+                logging.warning(
+                    "Non-standard XML format found for URL '{}'".format(url))
+                continue
 
-            if(feedFound):
-                days_inactive = days_since_last_update(feed, today)
-                if days_inactive >= minimum_days_inactive:
-                    if company in inactive_companies:
+            # Add company -> url pair to return dictionary if inactivity criterion is met. Also, prevent the a link from being added twice to the same key
+            if days_inactive >= minimum_days_inactive:
+                if company not in inactive_companies:
+                    inactive_companies[company] = [url]
+                # No duplicate URLs for the same company
+                else:
+                    if url not in inactive_companies[company]:
                         inactive_companies[company].append(url)
-                    else:
-                        inactive_companies[company] = [url]
-
     return inactive_companies
 
 
 def days_since_last_update(feed, today):
+    '''Takes a feed object and extracts the most recent published date.
+    The day difference between that date and 'today' is then returned. '''
 
-    # NEED TO CATCH IF DATE ITEM DOESNT EXIST LIKE WITH THAT ONE PODCAST
+    # Dates have a strange format. Split the items by spaces, then build a datetime object so we can do date arithmetic
 
-    # dates have a strange format, with items seperated by spaces. split the items by spaces, then build a datetime object so we can do date arithmetic
-    split_date = feed["feed"]["published"].split(" ")
+    try:
+        date_str = feed["feed"]["published"]
+    except (KeyError):
+        logging.warning(
+            "No publishing date found.")
+        return KeyError
+
+    split_date = date_str.split(" ")
     day = split_date[1]
     if len(day) == 1:
         day = "0" + day  # the number of digits on the day of the month is irregular, so add a leading zero if just one digit
     month = split_date[2]
     year = split_date[3]
     hour_minute_second = split_date[4]
-
     last_post_date = datetime.datetime.strptime(
         day + month + year + hour_minute_second, "%d%b%Y%H:%M:%S")  # build datetime object
 
@@ -74,6 +83,7 @@ def days_since_last_update(feed, today):
     return (today - last_post_date).days
 
 
+# Baseline type and value argument checks to make sure the function is being used as intended.
 def sanity_check(rss_dict, minimum_days_inactive):
     assert isinstance(
         rss_dict, dict), "Argument of wrong type! Type 'dictionary' required as first argument for fuction get_rss_inactivity"
@@ -96,7 +106,4 @@ def sanity_check(rss_dict, minimum_days_inactive):
                       int), "Argument of wrong type! Type 'int' required as second argument for fuction get_rss_inactivity"
 
     assert minimum_days_inactive >= 0, "Minimum days inactaive must be a non-negative integer"
-
-
-print(get_rss_inactivity(
-    {'Unraveled': ['https://rss.acast.com/unraveled']}, 10))
+    logging.info("Sanity Check Cleared.")
--- test_rss_inactivity.py
+++ test_rss_inactivity.py
@@ -1,21 +1,68 @@
 import unittest
+import datetime
 import rss_inactivity as rss
+
+
+link_dict = {
+    'Unraveled': ['https://rss.acast.com/unraveled'],
+    'Bible': ['https://feeds.fireside.fm/bibleinayear/rss'],
+    'Dateline': ['https://podcastfeeds.nbcnews.com/dateline-nbc']
+}
 
 
 class TestRSS(unittest.TestCase):
 
+    # Test sanity check of bad parameter values and types
+    def test_BadInput_AssertionErrorThrown(self):
+        with self.assertRaises(AssertionError):
+            # bad type arg1 key
+            rss.get_rss_inactivity({4: ["6"]}, 1)
+            # bad type arg1 value (no list)
+            rss.get_rss_inactivity({'a': "6"}, 1)
+            # bad type arg1 value(within list)
+            rss.get_rss_inactivity({'a': [9]}, 1)
+            # bad type arg1 value(empty list)
+            rss.get_rss_inactivity({'a': [9]}, 1)
+            # bad value arg2
+            rss.get_rss_inactivity({'a': ["6"]}, -1)
+            # bad type arg2
+            rss.get_rss_inactivity({'a': ["6"]}, "hello")
+
+    def test_ActiveFeed_TitleInReturnDict(self):
+        self.assertIn("Unraveled", rss.get_rss_inactivity(link_dict, 0))
+
+    def test_InactiveFeed_TitleNotInReturnDict(self):
+        self.assertNotIn(
+            "Unraveled", rss.get_rss_inactivity(link_dict, 1000000))
+
     def test_ReturnDictIsBlank_False(self):
-        self.assertNotEqual(rss.get_rss_inactivity({'a': ['1']}, 1), {})
+        self.assertNotEqual(rss.get_rss_inactivity(link_dict, 0), {})
 
-    # def test_BadInput_AssertionErrorThrown(self):
-    #     self.assertRaises(
-    #         AssertionError, rss.get_rss_inactivity({'a': ["6"]}, 1))
-    #     self.assertRaises(AssertionError, rss.get_rss_inactivity({[]: 5}, 1))
+    def test_ThreeDifferentLinksOneCompany(self):
+
+        _link_dict = {
+            'Unraveled': ['https://rss.acast.com/unraveled', 'https://feeds.fireside.fm/bibleinayear/rss', 'https://podcastfeeds.nbcnews.com/dateline-nbc'],
+            'Dateline': ['https://podcastfeeds.nbcnews.com/dateline-nbc']
+        }
+
+        today = datetime.datetime.strptime(
+            "08Feb202117:57:34", "%d%b%Y%H:%M:%S")  # manually set date so this test doesn't fail tomorrow :)
+
+        result = rss.get_rss_inactivity(_link_dict, 0, today=today)
+        self.assertEqual(len(result["Unraveled"]), 3)
+
+    def test_OneLinkTwoCompanies(self):
+
+        _link_dict = {
+            'Unraveled': ['https://podcastfeeds.nbcnews.com/dateline-nbc'],
+            'Dateline': ['https://podcastfeeds.nbcnews.com/dateline-nbc']
+        }
+        today = datetime.datetime.strptime(
+            "08Feb202117:57:34", "%d%b%Y%H:%M:%S")  # manually set date so this test doesn't fail tomorrow :)
+
+        result = rss.get_rss_inactivity(_link_dict, 0, today=today)
+        self.assertEqual(result["Unraveled"], result["Dateline"])
 
 
-    # TODO
-    # Test if urls and companies are paired correctly
-    # Test multiple feeds per company, some lapsed some not
-    # assert url regex
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(buffer=True)
